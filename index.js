f8a03f70570935b8033276a09215ee3f43daa1eb
Ismail El mouaddab <ismailelmouaddab@gmail.com>
2023-04-06 23:00:26 +0200

    corrections affichage

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const surfaceJeu = document.querySelector(".surface-jeu");
 const scoreElement = document.querySelector(".score");
-const recordElement = document.querySelector(".Record");
 const controles = document.querySelectorAll(".controles i");
 
 let serpentX = 5, serpentY = 5;
@@ -10,8 +9,6 @@ let vitesseX = 0, vitesseY = 0;
 let setIntervalId;
 let corpsSerpent = [];
 let score = 0;
-let Record = localStorage.getItem("Record") || 0;
-recordElement.innerText = 'Record :${Record}';
 
 const updatePomme = () => {
     pommeX = Math.floor(Math.random() * 30) + 1;
@@ -20,55 +17,55 @@ const updatePomme = () => {
 
 const GameOver = () => {
     clearInterval(setIntervalId);
-    alert("Fin du JEU! Appuyez sur Rejouer pour continuer");
+    alert("Fin du JEU! Appuyez sur OK pour continuer");
     location.reload();
 }
 
-const changementDirection = k => {
-    if (k.key === "FlecheHaut" && vitesseY != 1) {
+const changementDirection = e => {
+    if (e.key === "ArrowUp" && vitesseY != 1) {
         vitesseX = 0;
         vitesseY = -1;
-    } else if (k.key === "FlecheBas" && vitesseY != -1) {
+    } else if (e.key === "ArrowDown" && vitesseY != -1) {
         vitesseX = 0;
         vitesseY = 1;
-    } else if (k.key === "FlecheGauche" && vitesseX != 1) {
+    } else if (e.key === "ArrowLeft" && vitesseX != 1) {
         vitesseX = -1;
         vitesseY = 0;
-    } else if (k.key === "FlecheDroite" && vitesseX != -1) {
+    } else if (e.key === "ArrowRight" && vitesseX != -1) {
         vitesseX = 1;
         vitesseY = 0;
     }
 }
 
-controles.forEach(button => button.addEventListener("click", () => changementDirection({ key: button.dataset.key })));
+controles.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
 
 
 const initJeu = () => {
     if (finJeu) return GameOver();
-    let html = '<div class="pomme" style="grid-area:${pommeY}/${pommeX}"></div>';
+
+    let html = `<div class="pomme" style="grid-area: ${pommeY} / ${pommeX}"></div>`;
 
     if (serpentX === pommeX && serpentY === pommeY) {
         updatePomme();
-        corpsSerpent.push([pommeX, pommeY]);
+        corpsSerpent.push([pommeY, pommeX]);
         score++;
-        Record = score >= Record ? score : Record;
-        localStorage.setItem("Record", Record);
-        scoreElement.innerText = 'Score: ${score}';
-        recordElement.innerText = 'Record: ${Record}';
+        scoreElement.innerText = `Score: ${score}`;
     }
     serpentX += vitesseX;
     serpentY += vitesseY;
+
     for (let i = corpsSerpent.length - 1; i > 0; i--) {
         corpsSerpent[i] = corpsSerpent[i - 1];
     }
 
     corpsSerpent[0] = [serpentX, serpentY];
+
     if (serpentX <= 0 || serpentX > 30 || serpentY <= 0 || serpentY > 30) {
         return finJeu = true;
     }
 
     for (let i = 0; i < corpsSerpent.length; i++) {
-        html += 'div class="serpent" style="grid-area: ${corpsSerpent[i][1]}/${corpsSerpent[i][0]}"></div>';
+        html += `<div class="serpent" style="grid-area: ${corpsSerpent[i][1]} / ${corpsSerpent[i][0]}"></div>`;
         if (i !== 0 && corpsSerpent[0][1] === corpsSerpent[i][1] && corpsSerpent[0][0] === corpsSerpent[i][0]) {
             finJeu = true;
         }
